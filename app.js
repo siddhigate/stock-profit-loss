d61a7d63f40f2000638bd472bab467509260663f
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-08-15 18:44:20 +0530

    feat: added functionality and styling

---
+++ app.js
@@ -0,0 +1,111 @@
+const initial_price = document.querySelector("#initial_price");
+const current_price = document.querySelector("#current_price");
+const stock_quantity = document.querySelector("#stock_quantity");
+
+const submit_btn = document.querySelector("#submit_btn");
+
+const output = document.querySelector(".output_msg");
+const error = document.querySelector(".error-msg");
+
+const container = document.querySelector(".container");
+
+submit_btn.addEventListener("click", submitHandler);
+
+function submitHandler(){
+    
+    if(isValidInput()){
+        calculateProfitLoss();
+    }
+}
+
+function isValidInput(){
+    
+    if(initial_price.value === "" || current_price.value === "" || stock_quantity.value === ""){
+        setError("Please enter all the values");
+    }
+    else if(initial_price.value <= 0 || current_price.value <= 0 || stock_quantity.value <=0 ){
+        console.log("invalid")
+        setError("Please enter valid values. Values should be greater than zero");
+    }
+    else{
+        console.log("valid")
+        return true;
+    }
+}
+
+function setError(errorText){
+    error.style.display = "initial";
+    error.innerText = errorText;
+}
+
+function resetError(){
+    error.innerText = "";
+}
+
+function calculateProfitLoss(){
+
+    let init_price = initial_price.value;
+    let curr_price = current_price.value;
+    let quantity = stock_quantity.value;
+
+    if(init_price > curr_price){
+        
+        let loss = ((init_price - curr_price) * quantity).toFixed(2);
+        let loss_per = ((loss / init_price) * 100).toFixed(2);
+        setOutput("Loss", loss, loss_per);
+    }
+    else if(init_price < curr_price){
+        let profit = ((curr_price - init_price) * quantity).toFixed(2);
+        let profit_per = ((profit / init_price) * 100).toFixed(2);
+        setOutput("Profit", profit, profit_per);
+    }
+    else{
+        setOutput("Neutral");
+    }
+
+}
+
+function setOutput(status, amount, percentage){
+
+    switch (status) {
+        case "Profit":
+            container.style.background="#32Cd32";
+            output.innerText = `The profit is ${amount} and the profit percentage is ${percentage}`;
+            break;
+
+        case "Loss":
+            container.style.background="#EF4444";
+            output.innerText = `The loss is ${amount} and the loss percentage is ${percentage}`;
+            break;
+    
+        case "Neutral":
+            container.style.background="#FBBF24";
+            output.innerText = "You earned No profit No loss"
+            break;
+    
+        default:
+            break;
+    }
+
+    document.querySelector(".footer").scrollIntoView();
+}
+
+function resetOutput(){
+    container.style.background = "white";
+    output.innerText="";
+}
+
+initial_price.addEventListener("click", function(){
+    resetError();
+    resetOutput();
+})
+
+stock_quantity.addEventListener("click", function(){
+    resetError();
+    resetOutput();
+});
+
+current_price.addEventListener("click", function(){
+    resetError();
+    resetOutput();
+});
